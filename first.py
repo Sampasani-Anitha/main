1b0811bae56c82d7b8eefe2009c96c5c47eeeaa6
Sampasani-Anitha <sampasanianitha@gmail.com>
2024-12-11 21:04:36 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,94 @@
+'''a=input("enter the values")
+x,y,z = a.split(" ")
+sum = int(x)+int(y)+int(z)
+print(sum)'''
+# simple prigrams by using "split" and "sep" and "end"
+
+'''name=input("enter the name and age:")
+name,age=name.split(",")
+print("Name:",name,",Age:",age,sep="")'''
+
+
+'''n=int(input("enter the value of n: "))
+print("countdown:5 4 3 2 1 ", end="Blast off!")'''
+
+
+# if,elif and nested elif statements execution
+
+'''weather=input("enter the name of the weather:")
+
+if weather=="rainy":
+    print("GO to home!")
+    
+elif weather=="coldy":
+    print("wear the sweater:")
+#else:
+    print("do not wear the sweater")
+elif weather=="hot":
+    print("stay at home:")
+else:
+    print("donot stay at home:")'''
+
+
+'''weather=input("enter the which weather you want:")
+time_of_day=input("enter which day you want:")
+
+if weather == "sunny":
+    print("you can play with your toy:")
+elif weather =="rainy":
+    print("you can play with your boat toy")
+elif weather =="snowy" and time_of_day == "night":
+    print("you can play with your teddy bear:")
+else:
+    print("you can play with your snow man toy:")
+
+print("you can play with your dady:")'''
+
+
+
+
+# nestes elif statements
+'''weather="rainy"  
+time_of_day="night"
+if weather == "sunny":
+    if time_of_day == "morning":
+        print("you play with your cat:")
+    else:
+        print("it's a night. time to sleep:")
+elif weather =="rainy":
+    print("you can play with your boat toy")
+elif weather =="snowy":
+    if time_of_day =="night":
+          print("you can play with your teddy bear:")
+    else:
+          print("you can play with your snow man toy:")
+else:
+
+          print("you can play with your dady:")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
